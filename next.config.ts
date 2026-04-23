ba75bc2dc042701798cad6fdd74b0424538646e2
Yehor <egor.meho@gmail.com>
2026-04-23 10:56:26 -0600

    fix: allow local IP for next/image optimization

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   images: {
+    dangerouslyAllowLocalIP: true,
     remotePatterns: [
       {
         protocol: "https",
